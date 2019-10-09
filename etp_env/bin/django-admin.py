7a63f01934149f8f06ccf1cd360c3475abab84f8
Morgan Reilly <morgan.reilly27@gmail.com>
2019-10-09 14:46:11 +0100

    Development started
    using a Google Codelabs tutorial
    
    Issue with file paths
    
    Can't figure out how to add relative file paths, should have a fix by the end of the day

---
+++ etp_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/morgan/GMIT/modules/Emerging_Tech/ET_PROJECT_2019/etp_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

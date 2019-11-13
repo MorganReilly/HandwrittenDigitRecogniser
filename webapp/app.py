fb5b193c8fa48848e011bea40b692b90f83fa02d
Morgan Reilly <morgan.reilly27@gmail.com>
2019-11-13 16:21:37 +0000

    Image Conversion
    Working on converting the image from the canvas as base64 and then sending that to the model

--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import io, base64
+import re
+from PIL import Image
+
+# https://www.base64encoder.io/python/
+# https://stackoverflow.com/questions/41256733/regex-to-extract-multiple-base64-encoded-image-from-string
+# https://stackoverflow.com/questions/31410525/base64-uri-to-png-python
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def app_home():
-    return 'Emerging Technologies!'
-
-
-@app.route('/input')
-def input_digit():
     return render_template('mnist-input.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_digit():
+    print("HERE!")
+    # Store string thats sent from Ajax call
+    ajax_string = request.values[('imageString')]
+
+    # When the image is sent we need to remove the initial part
+    # of the message sent.
+
+    # print(ajax_string)  # Non regex
+
+    # using regex to cut first part of image data sent
+    image_data = re.sub('^data:image/.+;base64,', '', ajax_string)
+    print(image_data)
+
+    # Converting base64 to image
+    b64_bytes = base64.b64decode(image_data)
+    image = io.BytesIO(b64_bytes)
+
+    image.save("whatever.png")
+
+
+    # save image as a png
+
+    return "null"
 
 
 if __name__ == '__main__':
--- webapp/static/js/mnist-input.js
+++ webapp/static/js/mnist-input.js
@@ -1,89 +1,107 @@
 var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
 
-    var x = "white",
-        y = 8;
+var x = "white",
+    y = 8;
 
-    function init() {
-        canvas = document.getElementById('canvasMNIST');
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
+function init() {
+    canvas = document.getElementById('canvasMNIST');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
 
-        canvas.addEventListener("mousemove", function (e) {
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function erase() {
+    ctx.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+}
+
+// Sending the canvas image to Flask
+function publish() {
+    var canvas = document.getElementById("canvasMNIST");
+    var dataURL = canvas.toDataURL();
+
+    console.log("Data URL: " + dataURL);
+
+    // Adapted from: https://stackoverflow.com/questions/34779799/upload-base64-image-with-ajax
+    $.ajax({
+        url: "/upload",
+        type: "POST",
+        data: {imageString: dataURL}
+    }).done(function(e){
+        console.log("DONE");
+    })
+}
+
+
+function save() {
+    //Method #1
+    //document.getElementById("canvasimg").style.border = "2px solid";
+    //var dataURL = canvas.toDataURL();
+    //document.getElementById("canvasimg").src = dataURL;
+    //document.getElementById("canvasimg").style.display = "inline";
+
+    // Method #2
+    //var link = document.getElementId('link');
+    //link.setAttribute('download', 'MNIST.png');
+    //link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+    //link.click
+}
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
     }
-
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
+    if (res == 'up' || res == "out") {
+        flag = false;
     }
-
-    function erase() {
-        ctx.clearRect(0, 0, w, h);
-        document.getElementById("canvasimg").style.display = "none";
-    }
-
-    function save() {
-        //Method #1
-        //document.getElementById("canvasimg").style.border = "2px solid";
-        //var dataURL = canvas.toDataURL();
-        //document.getElementById("canvasimg").src = dataURL;
-        //document.getElementById("canvasimg").style.display = "inline";
-
-        // Method #2
-        //var link = document.getElementId('link');
-        //link.setAttribute('download', 'MNIST.png');
-        //link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-        //link.click
-    }
-
-    function findxy(res, e) {
-        if (res == 'down') {
+    if (res == 'move') {
+        if (flag) {
             prevX = currX;
             prevY = currY;
             currX = e.clientX - canvas.offsetLeft;
             currY = e.clientY - canvas.offsetTop;
-
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
+            draw();
         }
     }
+}
